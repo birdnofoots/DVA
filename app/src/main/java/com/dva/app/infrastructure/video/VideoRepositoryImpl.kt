6b6d749ec3e4c68692bb6fa9ef0189cf02e61444
birdnofeets <birdnofoots@gmail.com>
2026-04-03 01:02:41 +0000

    fix: add missing Bitmap import

--- app/src/main/java/com/dva/app/infrastructure/video/VideoRepositoryImpl.kt
+++ app/src/main/java/com/dva/app/infrastructure/video/VideoRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.dva.app.infrastructure.video
 
 import android.content.Context
 import android.database.Cursor
+import android.graphics.Bitmap
 import android.media.MediaCodec
 import android.media.MediaExtractor
 import android.media.MediaFormat
